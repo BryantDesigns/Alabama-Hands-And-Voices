bd02d18bd68699d865eec51ff1de892857bcae61
Tyler Bryant <tbryantdesignsllc@gmail.com>
2024-11-24 00:18:01 -0700

    Added ESLint, Prettier and Next.js configurations
    - Introduced ESLint configuration for better code quality checks
    - Added Prettier configuration to enforce consistent code style
    - Created a new Next.js configuration file to handle image loading from specific domains
    - Updated .gitignore file with common directories and files to ignore
    - Generated package-lock.json after installing project dependencies

---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        hvblue: {
+          DEFAULT: "#141B4B",
+          50: "#4859CE",
+          100: "#384BCA",
+          200: "#2E3EAC",
+          300: "#25328B",
+          400: "#1D276B",
+          500: "#141B4B",
+          600: "#080B1F",
+        },
+        hvorange: {
+          DEFAULT: "#FF7F32",
+          50: "#FFF2EA",
+          100: "#FFE5D5",
+          200: "#FFCBAC",
+          300: "#FFB284",
+          400: "#FF985B",
+          500: "#FF7F32",
+          600: "#F95D00",
+          700: "#C14800",
+          800: "#893300",
+          900: "#511E00",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
