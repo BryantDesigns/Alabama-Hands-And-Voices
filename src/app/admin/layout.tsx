197e42f7ae424b3012f4d556638ef6abb9a2200e
Tyler Bryant <tbryantdesignsllc@gmail.com>
2025-03-09 14:13:39 -0600

    Enhanced user authentication process
    The user authentication process has been improved by adding token expiration checks. Now, when a user's session is authenticated, the system retrieves the token details and checks if it's expired. If the token is expired or an error occurs during this check, the system automatically signs out the user and redirects to the authentication page. This change enhances security by ensuring that only users with valid tokens can access protected resources.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { onAuthStateChanged, signOut, User } from 'firebase/auth'
 import { auth } from '@/services/firebase/auth'
 import { useRouter } from 'next/navigation'
 
@@ -11,17 +11,39 @@ export default function AdminRouteLayout({
 }) {
     const router = useRouter()
     const [loading, setLoading] = useState(true)
-    const [user, setUser] = useState(null)
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            if (!currentUser) {
-                router.push('/auth') // Redirect if not authenticated
-            } else {
-                setUser(currentUser)
+        const unsubscribe = onAuthStateChanged(
+            auth,
+            async (currentUser: User | null) => {
+                if (currentUser) {
+                    try {
+                        // Retrieve token details.
+                        const tokenResult = await currentUser.getIdTokenResult()
+                        const expirationTime = new Date(
+                            tokenResult.expirationTime
+                        ).getTime()
+                        const currentTime = Date.now()
+
+                        // If the token is expired, sign out and redirect.
+                        if (expirationTime < currentTime) {
+                            await signOut(auth)
+                            router.push('/auth')
+                        } else {
+                            setLoading(false)
+                        }
+                    } catch (error) {
+                        console.error('Error checking token expiration:', error)
+                        // On error, force sign out and redirect.
+                        await signOut(auth)
+                        router.push('/auth')
+                    }
+                } else {
+                    router.push('/auth')
+                    setLoading(false)
+                }
             }
-            setLoading(false)
-        })
+        )
 
         return () => unsubscribe()
     }, [router])
